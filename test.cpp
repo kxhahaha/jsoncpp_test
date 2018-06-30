7c66bcf549636996c3cd8a520f6f5a06ae4c9a18
2544995178@qq.com <2544995178@qq.com>
2018-07-01 00:14:51 +0800

    add cmakelists.txt

--- test.cpp
+++ test.cpp
@@ -2,9 +2,38 @@
 #include<fstream>
 #include<errno.h>
 #include<syslog.h>
+#include<assert.h>
+
+#include "json.h"
+
+using namespace std;
+
+void cpp()
+{
+	ifstream ifs;
+	ifs.open("test.json");
+	assert(ifs.is_open());
+	
+	Json::Reader reader;
+	Json::Value root;
+	
+	if(!reader.parse(ifs, root, false))
+	{
+		cerr << "parse failed" << endl;
+		return;
+	}
+
+	string name = root["name"].asString();
+	cout << name <<endl;
+	int age = root["age"].asInt();
+	cout << age <<endl;
+
+}
+
+
 
 int main()
 {
-	
+	cpp();
 	return 0;
 }
